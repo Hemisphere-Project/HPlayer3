711538ee1a11b842c4d8c125f3d9ac1f1ff49014
Jeremie <jeremie@hemisphere-project.com>
2022-11-15 13:01:07 +0100

    Borne 2 ok

--- apps/2022_MHL3_Borne2_Garbeau/script.js
+++ apps/2022_MHL3_Borne2_Garbeau/script.js
@@ -7,11 +7,38 @@ $(function(){
   $('.page').hide()
   $('#page_home').show()
 
+  // GO TO
   $('.folder_icon').click(function(){
     var dest = $(this).attr("dest")
 
     $('#page_home').fadeOut(200)
     $("#"+dest).fadeIn(200)
+
+    if(dest=="page_vitrine"){
+      loadVitrine()
+    }
+
+
+    // VITRINE
+    function loadVitrine(){
+      $('.uppertitle').hide()
+      $('.cartel_content').hide()
+      $('.element').removeClass('selected')
+    }
+
+    // VITRINE SELECTOR
+    $('.element').click(function(){
+      // titles
+      $('.introtitle').hide()
+      $('.uppertitle').show()
+      // colors
+      $('.element').removeClass('selected')
+      $(this).addClass('selected')
+      // content
+      var dest = $(this).attr('dest')
+      $('.cartel_content').hide()
+      $('#'+dest).fadeIn(50)
+    })
 
   })
 
