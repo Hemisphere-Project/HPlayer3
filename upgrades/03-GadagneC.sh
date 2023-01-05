7c6aef28d9ff097e280e8448093e95dffe61202b
maigre <thomas@37m.gr>
2023-01-05 23:33:37 +0100

    upgrade 3

--- upgrades/03-GadagneC.sh
+++ upgrades/03-GadagneC.sh
@@ -22,6 +22,7 @@ pacman -Sy mpv --noconfirm
 pacman -S freetype2 fribidi fontconfig yasm git autoconf pkg-config libtool \
   lua luajit libvdpau libva libxv libjpeg libxkbcommon libxrandr libv4l libxss libcaca sdl2 \
   base-devel libx264 mesa fbida libbluray alsa-lib alsa-firmware ttf-roboto --noconfirm --needed
+pacman -S llvm-libs --noconfirm
 
 # Update HPLAYER3
 cd /opt/HPlayer3
