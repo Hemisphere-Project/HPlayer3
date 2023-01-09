96b1eb1072ef86ab1f16d8ccbd259791af3e8bbd
maigre <thomas@37m.gr>
2023-01-09 10:36:54 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
 
-# TODO
+# NodeJS
+pacman -S nodejs npm --noconfirm
+npm install -g n
+n stable
+hash -r
+npm install -g pm2
 
-system dependencies: 
-- nodejs
-- pigpio (apt / pikaur)
+# MPV (RC2)
+pacman -Sy mpv --noconfirm
+pacman -S freetype2 fribidi fontconfig yasm git autoconf pkg-config libtool \
+  lua luajit libvdpau libva libxv libjpeg libxkbcommon libxrandr libv4l libxss libcaca sdl2 \
+  base-devel libx264 mesa fbida libbluray alsa-lib alsa-firmware ttf-roboto --noconfirm --needed
+pacman -S llvm-libs --noconfirm
 
+# PIGPIO
+pikaur -Sy --noconfirm pigpio
+
+# INSTALL
+npm install
+
+# AUDIO
+cp ./asound.conf /etc/asound.conf
